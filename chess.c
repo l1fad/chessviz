d2ad336c13956b8a143e4532a63f12b190a2a623
Иванов Алексей Анатольевич <ip711s09@mwp05.csc.local>
2018-02-13 13:09:00 +0700

    taking board from any file

--- chess.c
+++ chess.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main()
-{
-    printf("8 r n b q k b n r\n7 p p p p p p p p\n6\n5\n4\n3\n2 P P P P P P P P\n1 R N B Q K B N R\n  a b c d e f g h\n");
+int main(int argc, char* argv[]){
+    int c;
+    FILE *file;
+    file = fopen(argv[1], "r");
+    if (file) {
+        while ((c = getc(file)) != EOF)
+            putchar(c);
+        fclose(file);
+    }
+    return 0;
 }
